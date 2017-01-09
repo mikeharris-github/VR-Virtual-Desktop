0c0b167baee60a58144174e61cc3a633b279d4ae
Mike Harris <mikeharris.mailbox@gmail.com>
2017-01-08 20:31:40 -0600

    1/8/2017 Commit

---
+++ Virtual Desktop/Assets/KeyInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class KeyInput : MonoBehaviour {
+
+    public SteamVR_TrackedObject left;
+    public SteamVR_TrackedObject right;
+
+
+	// Use this for initialization
+	void Start () {
+        //SteamVR.instance.overlay.ShowKeyboard(0, 0, "Description", 256, "", true,0);
+        SteamVR_Utils.Event.Listen("KeyboardCharInput", OnKeyboard);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var lDevice = SteamVR_Controller.Input((int)left.index); //create variable lDevice for left controller
+        var rDevice = SteamVR_Controller.Input((int)right.index); //create variable rDevice for right controller
+
+        if (lDevice != null && lDevice.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            SteamVR.instance.overlay.ShowKeyboard(0, 0, "Description", 256, "", true, 0);
+        }
+        if (lDevice != null && lDevice.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            SteamVR.instance.overlay.ShowKeyboard(0, 0, "Description", 256, "", true, 0);
+
+        }
+
+
+    }
+
+    private void OnKeyboard(object[] args)
+    {
+        VREvent_t evt = (VREvent_t)args[0];
+        Debug.Log ((char)evt.data.keyboard.cNewInput0);
+        VdmDesktopManager.Instance.SimulateKey((char)evt.data.keyboard.cNewInput0); //custom code utilizing Windows API
+        // WindowsInput.InputSimulator.SimulateKeyPress((WindowsInput.VirtualKeyCode)evt.data.keyboard.cNewInput0);
+        //InputSimulator.SimulateKeyPress(VirtualKeyCode.SPACE);
+        // Do something with the accumulated text here
+    }
+}
